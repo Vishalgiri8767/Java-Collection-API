6f8eeab4ff5a48c84366758e082bf19941cdce4b
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-03 11:16:35 +0530

    bubble sort searching algorithm using java

--- SortingAlgorithms/BubbleSort.java
+++ SortingAlgorithms/BubbleSort.java
@@ -6,8 +6,8 @@ public class BubbleSort {
             System.out.println("invalid array");
         }
         else {
-            for (int i = 0; i < arr.length - 1; i++) {
-                for (int j = i + 1; j < arr.length; j++) {
+            for (int i = 0; i < arr.length-1; i++) {
+                for (int j = i + 1; j < arr.length-i; j++) {
                     if (arr[i] > arr[j]) {
                         int temp = arr[i];
                         arr[i] = arr[j];
@@ -21,7 +21,7 @@ public class BubbleSort {
         };
     }
     public static void main(String[] args) {
-        int [] arr = {1,-14,0};
+        int [] arr = {1,-14,0,5};
         bubbleSort(arr);
     }
 }
