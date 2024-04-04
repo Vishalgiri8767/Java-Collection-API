79e0edb5b2539acd86508b74fd904e9bb2add142
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-04 23:35:35 +0530

    program for checking leap or not

--- BasicProblems/LeapYear.java
+++ BasicProblems/LeapYear.java
@@ -4,15 +4,23 @@ import java.util.Scanner;
 
 public class LeapYear {
     public static boolean checkLeapYear(int year){
-        if(year != 4)
-            return false;
 
-        return true;
+        if ((year%4==0 && year%100!=0) || (year%400==0)){
+           // System.out.println("leap year");
+            return true;
+        }
+        else
+            return false;
     }
     public static void main(String[] args) {
         System.out.println("Enter Year");
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
-        checkLeapYear(year);
+        boolean isLeapYear = checkLeapYear(year);
+        if(isLeapYear)
+            System.out.println(year+" is leap year");
+        else
+            System.out.println(year+" is not leap year");
+        System.out.println(isLeapYear);
     }
 }
