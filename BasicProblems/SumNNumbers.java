5deaa5fc4a9ea09adcb70ae2b2c4d30a58dacf17
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-06 00:28:32 +0530

    sum of first n natural numbers

---
+++ BasicProblems/SumNNumbers.java
@@ -0,0 +1,20 @@
+package BasicProblems;
+
+import java.util.Scanner;
+
+public class SumNNumbers {
+    public static int doSum(int number){
+        int sum = 0;
+        for(int i=1; i<=number; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int number = sc.nextInt();
+        int sum = doSum(number);
+        System.out.println(sum);
+    }
+}
