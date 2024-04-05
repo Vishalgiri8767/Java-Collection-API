0c4eb266aad5f66e464b77ee9c603469778b7820
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-05 17:26:22 +0530

    selection_sort algorithm

--- SortingAlgorithms/SelectionSort.java
+++ SortingAlgorithms/SelectionSort.java
@@ -1,15 +1,26 @@
 package SortingAlgorithms;
+
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args) {
-        int [] arr = {3,9,4,8,1};
-        selectionSort(arr);
-    }
-    public static void selectionSort(int arr[]){
-        int min = 0;
-        for(int i=1; i<arr.length; i++){
-            if(min>arr[i]){
-
-            }
-        }
+        int [] arr = {3,9,4,10,6,9,0};
+        int[] sortedArr=selectionSort(arr);
+        System.out.println(Arrays.toString(sortedArr));
+    };
+    public static int[] selectionSort(int[] arr){
+        int min,temp=0;
+        for(int i=0; i<arr.length; i++){
+            min=i;
+            for(int j=i+1; j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                };
+            };
+            temp = arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        };
+        return arr;
     }
 }
