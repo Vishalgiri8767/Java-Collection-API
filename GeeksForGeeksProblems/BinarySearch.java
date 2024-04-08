b145b833a3dc8bfac4317e71f80199e6dc8a3ce1
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-08 23:12:17 +0530

    binary search algorithm

---
+++ GeeksForGeeksProblems/BinarySearch.java
@@ -0,0 +1,18 @@
+package GeeksForGeeksProblems;
+//        Given a sorted array of size N and an integer K,
+//        find the position(0-based indexing) at which K is present
+//        in the array using binary search.
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {3,9,3,4,1,8,6};
+        int k=4;
+        BinarySearch bs = new BinarySearch();
+        bs.binarySearch(arr,k);
+    }
+    public int[] binarySearch(int[] arr, int k){
+
+        return arr;
+    }
+
+}
