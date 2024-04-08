93c4b21e584592687cd9ea34b4870de2d2d8ec30
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-08 22:58:00 +0530

    Searching an element in a sorted array

---
+++ GeeksForGeeksProblems/SearchElement.java
@@ -0,0 +1,20 @@
+package GeeksForGeeksProblems;
+
+public class SearchElement {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,6,8,9};
+        int N = arr.length;
+        int K = 84;
+        int result = searchInSorted(arr,N,K);
+        System.out.println(result);
+    }
+    static int searchInSorted(int arr[], int N, int K)
+    {
+
+        for(int i=0; i<N; i++){
+            if(arr[i]==K)
+                return 1;
+        }
+        return -1;
+    }
+}
