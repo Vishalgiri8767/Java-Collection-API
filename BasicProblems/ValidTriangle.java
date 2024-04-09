0d0363e23cfcecac86f3e9fe03f79cfb2ebe4ffe
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-09 10:57:06 +0530

    Check given triangle is valid or not

---
+++ BasicProblems/ValidTriangle.java
@@ -0,0 +1,28 @@
+package BasicProblems;
+// for valid triangle :
+// two sides sum > other side.
+
+import java.util.Scanner;
+
+public class ValidTriangle {
+    static boolean checkTriangle(int a,int b,int c){
+        if(a+b >c && a+c > b && b+c> a )
+            return true;
+
+        return false;
+    };
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of first side: ");
+        int a = sc.nextInt();
+        System.out.println("Enter value of second side: ");
+        int b = sc.nextInt();
+        System.out.println("Enter value of third side: ");
+        int c = sc.nextInt();
+        boolean isTriangel = checkTriangle(a,b,c);
+        if(isTriangel)
+            System.out.println("Valid Triangle");
+        else
+            System.out.println("Invalid triangle");
+    }
+}
