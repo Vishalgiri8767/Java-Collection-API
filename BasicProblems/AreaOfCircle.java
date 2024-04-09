79f69b8c4e6fca6352b399329b9450855061442b
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-09 12:04:21 +0530

    Area of circle

--- BasicProblems/AreaOfCircle.java
+++ BasicProblems/AreaOfCircle.java
@@ -2,14 +2,18 @@ package BasicProblems;
 
 public class AreaOfCircle {
     public double findArea(double r){
+        if(r <= 0)
+            return -1;
         double pie = 3.14;
         double area = pie*r*r;
         return area;
     }
     public static void main(String[] args) {
         AreaOfCircle ac = new AreaOfCircle();
-        int radius = 5;
+        double radius = 100;
         double area =ac.findArea(radius);
-        System.out.println("area :"+area);
+        String string = (area == -1) ? "Invalid radius" : "Area : "+area;
+
+        System.out.println(string);
     }
 }
