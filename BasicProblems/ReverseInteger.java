52b0f4ff13720e24ec14f1de0c1c23958855023e
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-09 12:38:06 +0530

    reverse number

--- BasicProblems/ReverseInteger.java
+++ BasicProblems/ReverseInteger.java
@@ -2,10 +2,21 @@ package BasicProblems;
 
 public class ReverseInteger {
     public static void main(String[] args) {
-       int revNum =  reverseInt(-2034);
+       int revNum =  reverseNum1(2034);
         System.out.println(revNum);
+        int rNum = reverseNum2(2034);
+        System.out.println(rNum);
     };
-    static int reverseInt(int num){
+    static int reverseNum2(int num){
+        String numStr = Integer.toString(num);
+        StringBuffer revStr = new StringBuffer();
+        for(int i=numStr.length()-1; i>=0; i--){
+            revStr.append(numStr.charAt(i));
+        };
+        int revNum = Integer.parseInt(revStr.toString());
+        return revNum;
+    }
+    static int reverseNum1(int num){
         int revNum = 0;
         while (num != 0){
             int digit = num%10;
