d9353294fe62f6c7c19f1441bcff289bf5d06668
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-12 23:18:51 +0530

    String operators operations

---
+++ StringProblems/Operaters.java
@@ -0,0 +1,18 @@
+package StringProblems;
+
+public class Operaters {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b');  // double quote for string and
+        // char a + char b             // single quote for char.
+        // it is printing there Integer value.(Ascii or unicode ).
+        // out put => 195
+
+        System.out.println("a" + "b");  //string a + string b. => string
+        System.out.println('a' + "bcd"); // string+ch => string
+
+        System.out.println('a' + 3);  // ch(ascii)+integer => integer.
+        System.out.println("a"+3);  // string+integer=>stringInteger
+
+
+    }
+}
