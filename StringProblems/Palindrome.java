0b049552bf1bf0eb0dc4985246f41ca9b8dc6bbb
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-13 15:00:54 +0530

    Java Program for checking string palindrome

--- StringProblems/Palindrome.java
+++ StringProblems/Palindrome.java
@@ -2,8 +2,10 @@ package StringProblems;
 // check first and last element if equal then shift first and last to i+1 and i(n-1) and check again.
 public class Palindrome {
     public static boolean checkPalindrome(String str){
-        str = str.toLowerCase();
 
+        if(str == null || str.length()==0)
+            return true;
+        str = str.toLowerCase();
         for (int i = 0; i < str.length() - 1; i++) {
             char start = str.charAt(i);
             char end = str.charAt(str.length() - 1 - i);
@@ -14,7 +16,7 @@ public class Palindrome {
         return true;
     };
     public static void main(String[] args) {
-        String str ="Abcba";
+        String str =null;
         boolean isPalindrome = checkPalindrome(str);
         System.out.println(isPalindrome);
     }
