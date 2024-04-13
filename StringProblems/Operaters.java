ec9ec9f184f9939e8f76d61811a748b3a8cca194
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-13 14:26:07 +0530

    String operator

--- StringProblems/Operaters.java
+++ StringProblems/Operaters.java
@@ -1,7 +1,15 @@
 package StringProblems;
-
 public class Operaters {
     public static void main(String[] args) {
+        printAtoZ();
+    };
+    public static void printAtoZ(){
+        for (int i = 0; i < 26; i++) {
+            char ch = (char)('A' + i);
+            System.out.println(ch);
+        }
+    }
+    public static void operators(){
         System.out.println('a' + 'b');  // double quote for string and
         // char a + char b             // single quote for char.
         // it is printing there Integer value.(Ascii or unicode ).
@@ -12,8 +20,5 @@ public class Operaters {
 
         System.out.println('a' + 3);  // ch(ascii)+integer => integer.
         System.out.println("a"+3);  // string+integer=>stringInteger
-
-
-
     }
 }
