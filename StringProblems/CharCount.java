72534e63bb89de69d530485725ebdb151972f951
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-16 23:50:20 +0530

    get distinct characters and their count in a string

--- StringProblems/CharCount.java
+++ StringProblems/CharCount.java
@@ -1,11 +1,24 @@
 package StringProblems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // How do you get distinct characters and their count in a string in Java?
 public class CharCount {
     public static void main(String[] args) {
-
+        String str = "aAaAbbCdea";
+        getCharCount(str);
     };
     public static void getCharCount(String str){
-
+        Map<Character, Integer> charsCount = new HashMap<>();
+        char[] chars = str.toCharArray();
+        for(char c : chars){
+            if(charsCount.containsKey(c)){
+                charsCount.put(c,charsCount.get(c)+1);
+            }
+            else
+                charsCount.put(c,1);
+        }
+        System.out.println(charsCount);
     }
 }
