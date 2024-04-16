6e612cf69837417c9b401706b6a3361377a44b58
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-16 23:46:12 +0530

    get distinct characters and their count in a string

--- StringProblems/CharCount.java
+++ StringProblems/CharCount.java
@@ -2,5 +2,10 @@ package StringProblems;
 
 // How do you get distinct characters and their count in a string in Java?
 public class CharCount {
+    public static void main(String[] args) {
 
+    };
+    public static void getCharCount(String str){
+
+    }
 }
