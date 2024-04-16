6554d5b3d55c7941047d8629bdf19bc79075ca10
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-16 23:13:02 +0530

    Remove white spaces from given string

--- StringProblems/RemoveSpaces.java
+++ StringProblems/RemoveSpaces.java
@@ -2,18 +2,21 @@ package StringProblems;
 
 public class RemoveSpaces {
     public static void main(String[] args) {
-        String str = "vishal giri ";
+        String str = "  vishal giri ";
         String s = removeSpaces(str);
         System.out.println(s.toString());
     }
     public static String removeSpaces(String str){
         StringBuilder sb = new StringBuilder();
+        String s = "";
         char[] ch = str.toCharArray();
         for(char letter: ch){
-            if(!Character.isWhitespace(letter))
+            if(!Character.isWhitespace(letter)) {
                 sb.append(letter);
-            str = String.valueOf(letter);
+                s+=letter;
+            }
         };
-        return str;
+        System.out.println(sb);
+        return s;
     }
 }
