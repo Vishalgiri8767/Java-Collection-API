af408cee6cd2c47c14ca5f4ffa905ba6b029e9ab
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-18 12:28:04 +0530

    Given five positive integers, find the minimum and maximum         values that can be calculated by summing exactly four         of the five integers.

--- HackerRank/MiniMaxSum.java
+++ HackerRank/MiniMaxSum.java
@@ -3,6 +3,11 @@ package HackerRank;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+/*Given five positive integers, find the minimum and maximum
+        values that can be calculated by summing exactly four
+        of the five integers. Then print the respective minimum
+        and maximum values as a single line of two space-separated
+        long integers.*/
 
 public class MiniMaxSum {
     public static MinMax findMinMaxSum(List<Integer> arr){
