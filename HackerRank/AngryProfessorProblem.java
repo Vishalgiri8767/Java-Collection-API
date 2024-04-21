9a443222f5214e23bb7c70e6fad302f1013f1199
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-21 13:54:08 +0530

    Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled.

---
+++ HackerRank/AngryProfessorProblem.java
@@ -0,0 +1,31 @@
+package HackerRank;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*Given the arrival time of each student and a threshhold number of attendees. determine if
+        the class is cancelled.
+        Example
+        a = [-2, -1,0, 1, 2]
+        The first 3 students arrived on. The last 2 were late. The threshold is 3 students, so class
+        will go on. Return YES.*/
+public class AngryProfessorProblem {
+    public static String angryProfessor(int k, List<Integer> a) {
+        int count = 0;
+        for (int i = 0; i <a.size() ; i++) {
+            if(a.get(i)<1){
+                count++;
+                if(count>=k)
+                    return "NO";
+            }
+        }
+        return "YES";
+    }
+    public static void main(String[] args) {
+        int k = 2;
+        List<Integer> a = new ArrayList<>(Arrays.asList(0, -1, 2, 1));
+        String result = angryProfessor(k,a);
+        System.out.println(result);
+    }
+}
