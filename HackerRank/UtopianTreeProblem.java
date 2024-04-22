a0fe84fd1e43fb23257ba150f981c06bd01476ae
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-22 20:35:48 +0530

    return  the height of the tree after the given number of cycles

--- HackerRank/UtopianTreeProblem.java
+++ HackerRank/UtopianTreeProblem.java
@@ -2,14 +2,13 @@ package HackerRank;
 
 public class UtopianTreeProblem {
     public static void main(String[] args) {
-        int result = utopianTree(0);
+        int result = utopianTree(5);
         System.out.println(result);
     }
     public static int utopianTree(int n) {
-        int result = 1;
-        if (n==0)
-            return 1;
-        for (int i = 1; i <= n; i++) {
+        int result = 0;
+
+        for (int i = 0; i <= n; i++) {
             if(i%2==0){
                 result +=1;
                 //System.out.println(result);
