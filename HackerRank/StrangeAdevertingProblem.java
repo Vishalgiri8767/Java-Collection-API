52c588a8eb5dde730c3a7b17ec3282779d7e51b0
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-27 23:19:04 +0530

    stranger adverting problem

--- HackerRank/StrangeAdevertingProblem.java
+++ HackerRank/StrangeAdevertingProblem.java
@@ -5,6 +5,21 @@ package HackerRank;
 
 public class StrangeAdevertingProblem {
     public static void main(String[] args) {
+        int result = viralAdvertising(5);
+        System.out.println(result);
+    }
+    public static int viralAdvertising(int n) {
+        // Write your code here
+        int liked = 0;
+        int shared = 5;
+        int cummulative = 0;
+        for (int day = 1; day <= n; day++) {
+            liked = shared/2;
+            shared = liked*3;
+            cummulative=liked+cummulative;
+        }
+        return cummulative;
 
     }
+
 }
