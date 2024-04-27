f1d9ab1747a3db5176f5bdfd8cf1a853893fc174
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-27 23:15:56 +0530

    stranger adverting problem

---
+++ HackerRank/StrangeAdevertingProblem.java
@@ -0,0 +1,10 @@
+package HackerRank;
+//HackerLand Enterprise is adopting a new viral advertising strategy.
+// When they launch a new product,
+// they advertise it to exactly  people on social media.
+
+public class StrangeAdevertingProblem {
+    public static void main(String[] args) {
+
+    }
+}
