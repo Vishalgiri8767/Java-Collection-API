c7c7416e433926a13511ed441c3f00670ad2c770
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-28 23:12:24 +0530

    loose coupling in java

--- JavaConcepts/LooseCoupling.java
+++ JavaConcepts/LooseCoupling.java
@@ -1,7 +1,30 @@
 package JavaConcepts;
 
+interface Computer {
+    void compile();
+};
+class Desktop1 implements Computer{
+    @Override
+    public void compile() {
+        System.out.println("compling in desktop");
+    }
+};
+class Laptop1 implements Computer{
+    @Override
+    public void compile() {
+        System.out.println("compling in Laptop");
+    }
+};
+class Programmer1{
+    public void code(Computer comp){
+        comp.compile();
+    }
+}
 public class LooseCoupling {
     public static void main(String[] args) {
+        Programmer1 p = new Programmer1();
+        p.code(new Desktop1());
+        p.code(new Laptop1());
 
     }
 }
