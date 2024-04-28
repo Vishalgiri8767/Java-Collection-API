c137cf265cf2b2aacaaa99beed33aca42e171d23
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-28 22:42:11 +0530

    tight coupling in java

--- JavaConcepts/TightCoupling.java
+++ JavaConcepts/TightCoupling.java
@@ -2,11 +2,15 @@ package JavaConcepts;
 
 public class TightCoupling {
     public static void main(String[] args) {
+        Programmer p = new Programmer();
+        p.coupling(new Laptop());
+     //   p.coupling(new Laptop());
+        Laptop lap = new Laptop();
 
     }
 }
 class Laptop{
-    public static void coupling(){
+    public void coupling(){
         System.out.println("Laptop class");
     }
 }
