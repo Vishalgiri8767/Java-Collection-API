40384494d7651e1c058838c7b8635ae966161d51
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-29 23:39:47 +0530

    return the minimum number of doses required, always 0 or more

--- HackerRank/hardleRaceProblem.java
+++ HackerRank/hardleRaceProblem.java
@@ -1,21 +1,20 @@
 package HackerRank;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class hardleRaceProblem {
     public static int hurdleRace(int k, List<Integer> height) {
-        // Write your code here
         int tl = Collections.max(height);
         if(k>=tl)
             return 0;
-
         return tl-k;
-
     }
-
-
     public static void main(String[] args) {
-
+        List<Integer> height = Arrays.asList(1,2,3,3,2);
+        int k =1;
+        int result = hurdleRace(k,height);
+        System.out.println(result);
     }
 }
