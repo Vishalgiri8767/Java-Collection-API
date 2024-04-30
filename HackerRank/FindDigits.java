ad9e1a79bdc46aa44c89a4db56b98ad05e97e78b
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-04-30 23:33:11 +0530

    Count the number of divisors occurring within the integer.

--- HackerRank/FindDigits.java
+++ HackerRank/FindDigits.java
@@ -2,5 +2,8 @@ package HackerRank;
 
 public class FindDigits {
 
-
+    public static void main(String[] args) {
+        int result = findNumber(12345);
+        System.out.println(result);
+    }
 }
